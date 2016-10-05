bb53ff2c327a69e9a437cd5522cc0bef23fbfdca
Derek Pryor <Derek.Pryor@jhuapl.edu>
2016-10-05 18:42:14 -0400

    Fixed bug when the token doesn't contain any roles

--- bossoidc/backend.py
+++ bossoidc/backend.py
@@ -101,10 +101,13 @@ def get_user_by_id(request, id_token):
 
     jwt = JWT().unpack(token).payload()
 
-    if 'realm_access' in jwt: # Session logins and Bearer tokens from password Grant Types
-        roles = jwt['realm_access']['roles']
-    else: # Bearer tokens from authorization_code Grant Types
-        roles = jwt['resource_access']['account']['roles']
+    try:
+        if 'realm_access' in jwt: # Session logins and Bearer tokens from password Grant Types
+            roles = jwt['realm_access']['roles']
+        else: # Bearer tokens from authorization_code Grant Types
+            roles = jwt['resource_access']['account']['roles']
+    except KeyError:
+        roles = [] # No roles assigned / contained in the token
 
     user.is_staff = 'admin' in roles or 'superuser' in roles
     user.is_superuser = 'superuser' in roles
